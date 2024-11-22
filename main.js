f368815e51ae1293dfb0a4d0ccab9f7c36db1ae3
Jully Achenchi <jullyachenchi8@gmail.com>
2024-11-22 11:33:15 +0300

    Modified website files

--- main.js
+++ main.js
@@ -44,6 +44,38 @@ function activeWork() {
 
 linkWork.forEach((a) => a.addEventListener('click', activeWork));
 
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_suimnoj','template_bzkrnso','#contact-form','uAIl64g81Xm7yxa9I')
+    .then(() =>{
+        //show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000)
+    
+        //clear input fields
+        contactForm.reset()
+    
+    }, () =>{
+        //show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+      
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
+
 // JS code to display views
 const counter = document.querySelector(".count-number");
 async function updateCounter() {
